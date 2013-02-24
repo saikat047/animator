74bd0269bc28430a4cef4bf57430da4efcc98571
Kazi Abdullah Saikat <kazi.saikat@bouvet.no>
2013-02-24 10:48:44 +0100

    First checkin

---
+++ src/main/java/com/fun/App.java
@@ -0,0 +1,13 @@
+package com.fun;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
