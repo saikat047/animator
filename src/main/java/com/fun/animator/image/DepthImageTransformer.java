7ad0722b3b895d8235b362d6c6996d390eecd8e6
Kazi Abdullah Saikat <kazi.saikat@bouvet.no>
2013-03-28 21:59:11 +0100

    Remove sysout

--- src/main/java/com/fun/animator/image/DepthImageTransformer.java
+++ src/main/java/com/fun/animator/image/DepthImageTransformer.java
@@ -4,10 +4,8 @@ import java.awt.image.BufferedImage;
 
 public interface DepthImageTransformer {
 
-    static final int DIFF_CENTIMETER = 0x80;
-    // minimum depth: in 100 cm
+    static final int DIFF_CENTIMETER = 0xA3;
     static final int DEPTH_MIN = 0x00000000;
-    // maximum depth: 511 cm
     static final int DEPTH_MAX = DepthImage.DEPTH_MASK;
 
     BufferedImage createColorImage(DepthImage depthImage);
